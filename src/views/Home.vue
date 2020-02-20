892dafeeffc53b72c5c168c5f106246a9a9765e0
Jeneen <jeneen.lemar@gmail.com>
2020-02-20 09:59:35 -0500

    adds new action to home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,20 @@
 <template>
   <div class="home">
+    <h1>New Movie</h1>
+    <div>
+      Title:
+      <input type="text" v-model="newMovieTitle"><br>
+      Year:
+      <input type="text" v-model="newMovieYear"><br>
+      Plot:
+      <input type="text" v-model="newMoviePlot"><br>
+      Director:
+      <input type="text" v-model="newMovieDirector"><br>
+      English (true or false):
+      <input type="text" v-model="newMovieEnglish"><br>
+      <button v-on:click="createMovie()">Save Movie</button>
+
+    </div>
     <h1>{{ message }}</h1>
     <h1>All Movies</h1>
     <div v-for="movie in movies">
@@ -18,7 +33,12 @@ export default {
   data: function() {
     return {
       message: "Movie App!",
-      movies: []
+      movies: [],
+      newMovieTitle: "",
+      newMovieYear: "",
+      newMoviePlot: "",
+      newMovieDirector: "",
+      newMovieEnglish: true
     };
   },
   created: function() {
@@ -26,7 +46,26 @@ export default {
       this.movies = response.data;
     });
   },
-  methods: {}
+  methods: {
+    createMovie: function() {
+      var params = {
+        title: this.newMovieTitle,
+        year: this.newMovieYear,
+        plot: this.newMoviePlot,
+        director: this.newMovieDirector,
+        english: this.newMovieEnglish
+      };
+      axios.post("/api/movies", params).then(response => {
+        this.movies.push(response.data);
+        this.newMovieTitle = "";
+        this.newMovieYear = "";
+        this.newMoviePlot = "";
+        this.newMovieDirector = "";
+        this.newMovieEnglish = "";
+      });
+
+    }
+  }
 };
 </script>
 
