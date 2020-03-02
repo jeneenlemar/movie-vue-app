ee011d350764ef7320487bc56cea1804b996bdec
Jeneen <jeneen.lemar@gmail.com>
2020-03-01 19:13:19 -0500

    adds theme to front end

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,81 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> |
-      <router-link to="/movies">Movie List</router-link> |
-      <router-link to="/movies/new">New Movie</router-link>| 
-      <router-link v-if="!isLoggedIn()" to="/signup">Signup</router-link> |
-      <router-link v-if="!isLoggedIn()" to="/login">Login</router-link> 
-      <router-link v-if="isLoggedIn()" to="/logout">Logout</router-link> |
-      
-      
+       <nav class="gtco-nav" role="navigation">
+        <div class="gtco-container">
+          <div class="row">
+            <div class="col-sm-2 col-xs-12">
+              <div id="gtco-logo"><a href="index.html">Movies!</a></div>
+            </div>
+            <div class="col-xs-10 text-right menu-1">
+              <ul>
+                <li class="active"><a href="index.html">Home</a></li>
+                <li></li>
+                <li><router-link  to="/movies">Movie List</router-link></li>
+                <li><router-link  to="/movies/new">New Movie</router-link></li>
+                <li><router-link v-if="!isLoggedIn()" to="/signup">Signup</router-link></li>
+                <li><router-link v-if="!isLoggedIn()" to="/login">Login</router-link></li>
+                <li><router-link v-if="isLoggedIn()" to="/logout">Logout</router-link></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+       </nav>
+      <div class="container">
+         <router-view/>
+      </div>
     </div>
-    <router-view/>
+   <footer id="gtco-footer" class="gtco-section" role="contentinfo">
+    <div class="gtco-container">
+      <div class="row row-pb-md">
+        <div class="col-md-4 gtco-widget gtco-footer-paragraph">
+          <h3>Movies!</h3>
+          <p>All you need to know about movies! </p>
+        </div>
+        <div class="col-md-4">
+          <div class="row">
+            <div class="col-md-6 gtco-footer-link">
+              <h3>Links</h3>
+              <ul class="gtco-list-link">
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Features</a></li>
+                <li><a href="#">Products</a></li>
+                <li><a href="#">Testimonial</a></li>
+                <li><a href="#">Contact</a></li>
+              </ul>
+            </div>
+            
+          </div>
+        </div>
+        <div class="col-md-4 gtco-footer-subscribe">
+          <form class="form-inline">
+            <div class="form-group">
+              <label class="sr-only" for="exampleInputEmail3">Email address</label>
+              <input type="email" class="form-control" id="" placeholder="Email">
+            </div>
+            <button type="submit" class="btn btn-primary">Send</button>
+          </form>
+        </div>
+      </div>
+    </div>
+    <div class="gtco-copyright">
+      <div class="gtco-container">
+        <div class="row">
+          <div class="col-md-6 text-left">
+            <p><small>&copy; 2016 Free HTML5. All Rights Reserved. </small></p>
+          </div>
+          <div class="col-md-6 text-right">
+            <p><small>Designed by <a href="http://freehtml5.co/" target="_blank">FreeHTML5.co</a> Demo Images: <a href="http://pixeden.com/" target="_blank">Pixeden</a> &amp; <a href="http://unsplash.com" target="_blank">Unsplash</a></small> </p>
+          </div>
+        </div>
+      </div>
+    </div>
+   </footer>
   </div>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
 <script>
 
--- src/views/MoviesIndex.vue
+++ src/views/MoviesIndex.vue
@@ -2,6 +2,92 @@
 
 <template>
   <div class="movies-index">
+
+    <!-- begin pasted info section -->
+    <div class="gtco-section">
+      <div class="gtco-container">
+        <div class="row">
+          <div class="col-md-8 col-md-offset-2 gtco-heading text-center">
+            <h2>Movie List</h2>
+            <p>See the movies below!</p>
+          </div>
+        </div>
+        <div class="row">
+
+          <div class="col-md-12">
+            <div class="owl-carousel owl-carousel-carousel">
+              <div class="item">
+                <div class="gtco-item">
+                  <a href="#"><img src="images/img_1.jpg" alt="" class="img-responsive" ></a> 
+                  <h2><a href="#">New York Arena</a></h2> 
+                  <p class="role">New York</p>
+                </div>
+              </div>
+              <div class="item">
+                <div class="gtco-item">
+                  <a href="#"><img src="images/img_2.jpg" alt="" class="img-responsive"></a>
+                  <h2><a href="#">Eagle Park</a></h2>
+                  <p class="role">London</p>
+                </div>
+              </div>
+              <div class="item">
+                <div class="gtco-item">
+                  <a href="#"><img src="images/img_3.jpg" alt="" class="img-responsive"></a>
+                  <h2><a href="#">Nationals Park</a></h2>
+                  <p class="role">Paris, France</p>
+                </div>
+              </div>
+
+              <div class="item">
+                <div class="gtco-item">
+                  <a href="#"><img src="images/img_1.jpg" alt="" class="img-responsive"></a>
+                  <h2><a href="#">New York Arena</a></h2>
+                  <p class="role">New York</p>
+                </div>
+              </div>
+              <div class="item">
+                <div class="gtco-item">
+                  <a href="#"><img src="images/img_2.jpg" alt="" class="img-responsive"></a>
+                  <h2><a href="#">Eagle Park</a></h2>
+                  <p class="role">London</p>
+                </div>
+              </div>
+              <div class="item">
+                <div class="gtco-item">
+                  <a href="#"><img src="images/img_3.jpg" alt="" class="img-responsive"></a>
+                  <h2><a href="#">Nationals Park</a></h2>
+                  <p class="role">Paris, France</p>
+                </div>
+              </div>
+
+              <div class="item">
+                <div class="gtco-item">
+                  <a href="#"><img src="images/img_1.jpg" alt="" class="img-responsive"></a>
+                  <h2><a href="#">New York Arena</a></h2>
+                  <p class="role">New York</p>
+                </div>
+              </div>
+              <div class="item">
+                <div class="gtco-item">
+                  <a href="#"><img src="images/img_2.jpg" alt="" class="img-responsive"></a>
+                  <h2><a href="#">Eagle Park</a></h2>
+                  <p class="role">London</p>
+                </div>
+              </div>
+              <div class="item">
+                <div class="gtco-item">
+                  <a href="#"><img src="images/img_3.jpg" alt="" class="img-responsive"></a>
+                  <h2><a href="#">Nationals Park</a></h2>
+                  <p class="role">Paris, France</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          
+        </div>
+      </div>
+    </div>
+    <!-- end pasted info section -->
     
     <h1>{{ message }}</h1>
     <div v-for="movie in movies" v-on:click="currentMovie = movie" v-bind:class="{ selected: movie === currentMovie }">
@@ -15,11 +101,11 @@
 </template>
 
 <style>
-.selected {
+/*.selected {
   color: white;
   background-color: grey;
   
-}
+}*/
 </style>
 
 <script>
